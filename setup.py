0e33c6fe0eebb64e2c7c89f86bd1e25b9e34d883
SakifKhan98 <sakifkhan98@gmail.com>
2024-11-16 02:29:27 -0600

    Project setup initiated

--- setup.py
+++ setup.py
@@ -1,40 +1,39 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 
-# Read requirements.txt
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
 
-# Read README.md for long description
-with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()
+# Read the requirements.txt file and add the dependencies to the install_requires list
+with open("requirements.txt") as fh:
+    required = fh.read().splitlines()
+
+# Readme.md file will be used as the long description
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
-    name="mlops_modular_project",
+    name="mlops_modular_workflow",
     version="0.1.0",
-    author="Your Name",
-    author_email="yourmail@example.com",
-    description="A modular MLOps Pipeline Project",
+    author="Sakif Khan",
+    author_email="sakifkhan98@gmail.com",
+    description="A modular workflow for MLOps",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="your-github-repo",
+    url="https://github.com/SakifKhan98/mlops-modular-workflow",
     packages=find_packages(),
     classifiers=[
-        "Development Status :: 3 - Beta",
-        "Intended Audience :: ML Engineers",
-        "Programming Language :: Python >= 3.8",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.8",
     install_requires=required,
     extras_require={
-        'dev': [
-            'pytest>=7.1.1',
-            'pytest-cov>=2.12.1',
-            'flake8>=3.9.0',
-            'black>=22.3.0',
-            'isort>=5.10.1',
-            'mypy>=0.942',
+        "dev": [
+            "pytest>=7.1.1",
+            "pytest-cov>=2.12.1",
+            "flake8>=3.9.0",
+            "black>=22.3.0",
+            "isort>=5.10.1",
+            "mypy>=0.942",
         ],
-    }
+    },
 )
-
